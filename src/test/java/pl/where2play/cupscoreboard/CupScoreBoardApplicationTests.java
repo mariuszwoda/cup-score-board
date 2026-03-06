4a877539203e303f434050c61832a0e0952ec0c9
mariusz woda <mariusz.woda@gmail.com>
2026-03-06 01:00:30 +0100

    TASK-1 initial commit — project scaffold (Spring Initializr, pom.xml, gitignore)

---
+++ src/test/java/pl/where2play/cupscoreboard/CupScoreBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.where2play.cupscoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupScoreBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
